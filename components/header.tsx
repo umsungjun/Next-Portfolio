4b0599b6b95410c35f642ef81c4fc4bb40c2f834
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-02 15:10:31 +0900

    header link 설정

---
+++ components/header.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <Link href="/">
+            <span className="ml-3 text-xl">SungJun's Portfolio</span>
+          </Link>
+        </div>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link href="/" className="mr-5 hover:text-gray-900">
+            홈
+          </Link>
+          <Link href="/projects" className="mr-5 hover:text-gray-900">
+            프로젝트
+          </Link>
+          <Link
+            href="https://github.com/umsungjun"
+            className="mr-5 hover:text-gray-900"
+          >
+            깃허브
+          </Link>
+          <Link
+            href="https://developer-sungjun.tistory.com/"
+            className="mr-5 hover:text-gray-900"
+          >
+            블로그
+          </Link>
+          <Link
+            href="https://open.kakao.com/o/s9inHFnf"
+            className="mr-5 hover:text-gray-900"
+          >
+            연락하기
+          </Link>
+        </nav>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Button
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+}
