178eb0ff322e83b122a2e807097e3f571ab851f7
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-03 16:41:45 +0900

    img도메인 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "images.unsplash.com",
+      "www.notion.so",
+    ],
+  },
+};
 
 module.exports = nextConfig;
